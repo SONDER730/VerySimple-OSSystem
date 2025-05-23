63b8353ea22468ffc6b4e6efdf4a429cfb58cd66
SONDER730 <3285661389@qq.com>
2025-05-23 18:13:52 +0800

    kernel organized

--- src/kernel/main.asm
+++ src/kernel/main.asm
@@ -1,51 +1,41 @@
-org 0x7c00
+org 0x0
 bits 16
 
 
 %define ENDL 0x0D,0x0A
 
 start: 
-	jmp main
+	;print
+	mov si,msg_hello
+	call puts
+
+.halt:
+	cli
+	hlt
+;print a string to the screen
+; params:
+; ds:si points to string
 
 puts:
 	push si
 	push ax
+	push bx
 	
 .loop:
 	lodsb
 	or al,al
 	jz .done
 
-	mov ah,0x0e    ;calls bios interupt
+	mov ah,0x0E    ;calls bios interupt
+	mov bh,0
 	int 0x10
+	
 	jmp .loop
 
 .done:
+	pop bx
 	pop ax
 	pop si
 	ret
-	
-main:
-	;set data segment
-	mov ax,0
-	mov ds,ax
-	mov es,ax
 
-	;setup stack
-	mov ss,ax
-	mov sp,0x7c00
-
-	;print
-	mov si,msg_hello
-	call puts
-	hlt
-
-.halt:
-	jmp .halt
-
-msg_hello:db 'hello world!',ENDL,0
-
-
-
-times 510-($-$$) db 0
-dw 0AA55h
+msg_hello: db 'Hello world from KERNEL!', ENDL, 0
