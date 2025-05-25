ff8fceacd73c6f8413e1ea9ab93214b1689d202d
SONDER730 <3285661389@qq.com>
2025-05-25 13:36:00 +0800

    printf Function

--- src/bootloader/stage2/main.c
+++ src/bootloader/stage2/main.c
@@ -4,5 +4,8 @@
 void _cdecl cstart_(uint16_t bootDrive)
 {
     puts("HELLO WORLF FROM C!");
+    printf("Formatted %% %c %s\r\n",'a',"string");
+    printf("Formatted %d %i %x %p %o\r\n",1234,-5678,0xdead,0xbeef,012345,(short)27,(short)-42,(unsigned char)20,(char)-10);
+    printf("Formatted %6d %lx %lld %llx\r\n",-10000000001,0xdeadbeeful,012340005000,0xddddeebdull);
     for(;;);
 }
